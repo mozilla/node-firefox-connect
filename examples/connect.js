f72303ce560a6e027394ca239f9e66ed069654e6
Brittany Storoz <brittanystoroz@gmail.com>
2015-01-13 16:28:34 -0500

    Add example

---
+++ examples/connect.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var connect = require('../index');
+
+connect({ port: 1234, connect: true }, function(err, sim) {
+  var client = sim.client;
+
+  // Let's show for example all the running apps
+  client.getWebapps(function(err, webapps) {
+    webapps.listRunningApps(function(err, apps) {
+      console.log("Running apps:", apps);
+    });
+  });
+});
